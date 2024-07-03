b1ce47613f7abfb88b3fb1e7fe156ae05e1afb9b
Vinicius Nascimento <vini.a.nasci@gmail.com>
2024-07-03 14:05:14 -0300

    feat: add components

---
+++ src/app/components/big-card/big-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-big-card',
+  templateUrl: './big-card.component.html',
+  styleUrl: './big-card.component.css'
+})
+export class BigCardComponent {
+  titleNew:string = "Nova versão angular"
+  descriptionNew:string = "as19d19a1d9a1d9a1s9d19a1d9a19d1a91d"
+}
